45f14b26673f236f6381bf588e99187f560285b7
Eric Bujeque <eric@keblarsolutions.com>
2022-08-28 22:39:51 +0200

    Fix Robots

--- setup.py
+++ setup.py
@@ -5,23 +5,23 @@ setup(
     name='kioblog',
     # packages=['kioblog'],
     packages=find_packages(exclude=('kioblogdev', 'media')),
-    version='v0.1-alpha',
+    version='v0.1.1-alpha',
     license='MIT',
     description='Simple blog for Django',
     author='Eric Bujeque',
     author_email='noikzyr3@gmail.com',
-    url='https://github.com/user/reponame',
-    download_url='https://github.com/user/reponame/archive/v_01.tar.gz',
+    url='https://github.com/Eric-Bujeque/kioblog',
+    download_url='https://github.com/Eric-Bujeque/kioblog/archive/refs/tags/v0.1-alpha.tar.gz',
     keywords=['blog', 'django'],
     install_requires=[
         'Django>=3.0',
-        'django-robots>=5.0',
+        'django-robots>=4.0',
         'django-summernote>=0.8.20.0'
     ],
     classifiers=[
         'Development Status :: 3 - Alpha',  # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable"
         'Intended Audience :: Developers',
-        'Topic :: Software Development :: Blog',
+        'Topic :: Software Development :: Libraries :: Python Modules',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.4',
